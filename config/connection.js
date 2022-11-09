4b1032549ff5fe7431c9f5fb5c03aa60b1cf1764
Richard Hong <rhong24@gmail.com>
2022-11-08 18:17:42 -0600

    modifying host for deployment on heroku

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: '127.0.0.1',
+    host: process.env.HOST || 'localhost',
     dialect: 'mysql',
     port: 3306
   }
